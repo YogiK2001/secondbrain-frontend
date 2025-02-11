36dfc02f985b8189fc16096ee2a97d07cdf92954
YogiK2001 <kaustubh7718@gmail.com>
2025-02-11 19:41:24 +0530

    addded structure

---
+++ public/img/index.js
@@ -0,0 +1,19 @@
+import ideasFlow from './undraw_ideas-flow_8d3x.svg';
+import myAnswer from './undraw_my-answer_dwu4.svg';
+import personalNotes from './undraw_personal-notes_n75r.svg';
+import questions from './undraw_questions_g2px.svg';
+import softwareEngineer from './undraw_software-engineer_xv60.svg';
+import takingNotes from './undraw_taking-notes_4si1.svg';
+import thoughtProcess from './undraw_thought-process_pavs.svg';
+
+
+// Alternative object export syntax:
+export default {
+    ideasFlow,
+    myAnswer,
+    personalNotes,
+    questions,
+    softwareEngineer,
+    takingNotes,
+    thoughtProcess
+};
---
+++ src/components/ui/index.js
@@ -0,0 +1,11 @@
+// Import components from the ui directory
+import { Button } from './Button'
+import { Card } from './Card'
+import { Nav } from './Nav'
+
+// Export all components
+export {
+    Button,
+    Card,
+    Nav
+}
